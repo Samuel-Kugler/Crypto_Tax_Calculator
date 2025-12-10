5652c4ff79398f6161750d0a13b9de79d66b6ee7
Samuel-Kugler <samuel-kugler@gmx.de>
2025-12-10 11:26:21 +0100

    fixed some bugs and implemented a route for transactions

--- app/main.py
+++ app/main.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
 from app.routes.wallet_routes import router as wallet_router
+from app.routes.transaction_routes import router as transaction_router
+from app.repositories.db import Base, engine  # noqa
+from app.models import wallet, transaction  # noqa
 
 #uvicorn app.main:app --reload
 app = FastAPI()
 app.include_router(wallet_router)
+app.include_router(transaction_router)
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -14,6 +18,8 @@ def index():
     <h1>Welcome!</h1>
     <p>
         <a href="http://127.0.0.1:8000/wallets">➡️ Zu den Wallets</a>
+        <br>
+        <a href="http://127.0.0.1:8000/transactions">➡️ Zu den Transactions</a>
     </p>
     """
 
